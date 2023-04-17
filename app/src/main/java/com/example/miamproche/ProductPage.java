df4d3d18e29ead8601968dc2ebb721337ac9d2ed
Timothé <timothe.perier2403@gmail.com>
2023-04-17 18:20:50 +0200

    Product Page

--- app/src/main/java/com/example/miamproche/ProductPage.java
+++ app/src/main/java/com/example/miamproche/ProductPage.java
@@ -7,12 +7,19 @@ import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.text.Layout;
+import android.widget.Adapter;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -27,8 +34,8 @@ public class ProductPage extends AppCompatActivity {
             set_img_url(R.id.imageView_pp, "https://img.freepik.com/icones-gratuites/utilisateur_318-159711.jpg");
             set_txt(R.id.textView_description, "Description :\nLorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem");
             set_txt(R.id.textView_price, "Lorem ipsum dolor si €/kg");
-            set_txt(R.id.textView_producer_description, "Je m'appelle Réné, je cultive des légumes de saison avec des pratiques agricoles durables et respectueuses de l'environnement pour produire des légumes saint et bon. J'aime vendre localement pour nouer des liens et réduire mon empreinte carbone. Je m'efforce de fournir une expérience agricole unique et de qualité à mes clients.");
-
+            set_txt(R.id.textView_producer_description, "Je m'appelle Réné, je cultive des légumes de saison avec des pratiques agricoles durables et respectueuses de l'environnement pour produire des légumes saint et bon.");
+            set_suggestion();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -55,16 +62,38 @@ public class ProductPage extends AppCompatActivity {
         });
     }
 
-    protected void set_img(int id, Bitmap img) throws RuntimeException {
-
-        ImageView img_view= (ImageView) findViewById(id);
+    protected void set_img(int id, Bitmap img){
+        ImageView img_view= findViewById(id);
         img_view.setImageBitmap(img);
     }
 
     protected void set_txt(int id, String str)throws IOException{
-        TextView pricetxt = (TextView)findViewById(id);
+        TextView pricetxt = findViewById(id);
         pricetxt.setText(str);
     }
 
+    protected void set_suggestion() throws IOException {
+        // automation was too hard sorry
+        String[][] data = new String[][]{
+                {"https://img-3.journaldesfemmes.fr/D2DNQB0aIz4Bf_qiYbcl5xMNNa4=/1500x/smart/b9339668d757434c893f1a26bd114b3b/ccmcms-jdf/25934728.jpg", "patate_douce"},
+                {"https://cdn-s-www.ledauphine.com/images/0D290058-E714-4F25-9F8F-BDEAAA15ABEB/NW_raw/ce-sont-les-glucides-qui-conferent-a-la-fraise-son-gout-delicatement-sucre-photo-by-natasha-skov-on-unsplash-1648565758.jpg", "friases"},
+                {"https://img.passeportsante.net/1200x675/2021-05-03/i102200-vin.webp", "Vin Rouge"}
+        };
+        int counter = 1;
+        int id = 0;
+        String VS = "view_suggestion";
+        String tmp = "";
+
+        for(String[] S : data){
+            tmp = VS + counter +"_IV";
+            id = this.getResources().getIdentifier(tmp, "id", this.getPackageName());
+            System.out.println("test:"+id);
+            set_img_url(id, S[0]);
+            tmp = VS + counter + "_TV";
+            id = this.getResources().getIdentifier(tmp, "id", this.getPackageName());
+            set_txt(id, S[1]);
+            counter++;
+        }
+    }
 
 }
